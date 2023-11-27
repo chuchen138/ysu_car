05cd955a4e83745d728ef6d69abbe21c8bd0f468
ceekinbox <1040701718@qq.com>
2023-11-27 11:41:24 +0800

    dkm usart2 printf

--- HARDWARE/bluetooth_wifi/bluetooth_wifi.h
+++ HARDWARE/bluetooth_wifi/bluetooth_wifi.h
@@ -5,7 +5,11 @@
 
 #include "stdarg.h"
 #include "string.h"
-
+/** ************************
+  * @用途  初始化串口2
+  * @参数  bound 波特率 115200
+  * @返回值 void
+  * ************************/
 void USART2_Init(u32 bound);
 void u2printf(char* fmt, ...);
 #endif 
